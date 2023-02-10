58b159232eff36d09bca40814cad4a6613ad1a18
ViniciusPelizzari <vinipelizzari2410@gmail.com>
2023-02-10 14:34:40 -0300

    Variáveis - declaração e apresentação.

---
+++ Principal.java
@@ -0,0 +1,18 @@
+/**
+ * @author Vinícius Pelizzari
+ */
+public class Principal {
+
+    // imprimindo os exemplos dos tipos de dados
+    public static void main(String[] args) {
+        Variaveis vari = new Variaveis();
+        System.out.println("int: " + vari.retNum());
+        System.out.println("long: " + vari.retNumMaior());
+        System.out.println("double: " + vari.retVlrDec());
+        System.out.println("boolean: " + vari.retVerif());
+        System.out.println("String: " + vari.retTxt());
+        System.out.println("float" + vari.retVlrDeci());
+        System.out.println("short: " + vari.retVlrDecim());
+        System.out.println("byte: " + vari.retNumOne());
+    }
+}
---
+++ Variaveis.java
@@ -0,0 +1,48 @@
+/**
+ * @author Vinícius Pelizzari
+ */
+public class Variaveis {
+
+    // criando variáveis
+    private int num;
+    private long numMaior;
+    private double vlrvDec;
+    private boolean verif;
+    private String txt;
+    private float vlrDeci;
+    private short vlrDecim;
+    private byte numOne;
+
+    // criando métodos e declarando variáveis
+    public int retNum() {
+        return 33;
+    }
+
+    public long retNumMaior() {
+        return 1535486;
+    }
+
+    public double retVlrDec() {
+        return 4.4;
+    }
+
+    public boolean retVerif() {
+        return true;
+    }
+
+    public String retTxt() {
+        return "Teste";
+    }
+
+    public float retVlrDeci() {
+        return 5;
+    }
+
+    public short retVlrDecim() {
+        return 66;
+    }
+
+    public byte retNumOne() {
+        return 3;
+    }
+}
